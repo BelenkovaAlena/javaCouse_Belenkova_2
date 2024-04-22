72550e151347735122484a2cd9a7943a0b87c288
user <user@24-003057.iteco-inno.ru>
2024-04-22 14:18:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 2. Символ - секция на трибуне, число - место, число - стоимость билета.
+         char section = 'B';
+         int seat = 125;
+         int price = 2800;
+         System.out.printf("Section: %c%nSeat: %d%nPrice: %d", section, seat, price);
+    }
+}
